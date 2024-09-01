ae2c12eb2d2d073e8b61c4b5636020188a52b0e2
nextonesfaster <nextonesfaster@gmail.com>
2024-09-01 23:18:09 +0530

    add problems 41-43

---
+++ problems/problem_41.cpp
@@ -0,0 +1,31 @@
+// https://neetcode.io/problems/linked-list-cycle-detection
+
+struct Node
+{
+    int val;
+    Node *next;
+    Node() : val(0), next(nullptr) {}
+    Node(int x) : val(x), next(nullptr) {}
+    Node(int x, Node *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    bool hasCycle(Node *head)
+    {
+        Node *slow = head;
+        Node *fast = head;
+
+        while (fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast)
+                return true;
+        }
+
+        return false;
+    }
+};
