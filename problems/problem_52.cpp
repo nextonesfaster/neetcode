c5a85c5d8efcfa6aada0595732ada5597e4c06b7
nextonesfaster <nextonesfaster@gmail.com>
2024-09-05 13:38:39 +0530

    add problems 52 & 53

---
+++ problems/problem_52.cpp
@@ -0,0 +1,31 @@
+// https://neetcode.io/problems/lowest-common-ancestor-in-binary-search-tree
+
+#include <stack>
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution
+{
+public:
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+    {
+        while (true)
+        {
+            if (root->val < p->val && root->val < q->val)
+                root = root->right;
+            else if (root->val > p->val && root->val > q->val)
+                root = root->left;
+            else
+                return root;
+        }
+    }
+};
---
+++ problems/problem_53.cpp
@@ -0,0 +1,53 @@
+// https://neetcode.io/problems/level-order-traversal-of-binary-tree
+
+#include <vector>
+#include <queue>
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    std::vector<std::vector<int>> levelOrder(TreeNode *root)
+    {
+        if (!root)
+            return {};
+
+        std::vector<std::vector<int>> res;
+        std::queue<TreeNode *> queue;
+        queue.push(root);
+
+        while (!queue.empty())
+        {
+            std::vector<int> level;
+            int n = queue.size();
+            level.reserve(n);
+
+            for (int i = 0; i < n; i++)
+            {
+                TreeNode *node = queue.front();
+                queue.pop();
+                level.push_back(node->val);
+                if (node->left)
+                    queue.push(node->left);
+                if (node->right)
+                    queue.push(node->right);
+            }
+
+            res.push_back(level);
+        }
+
+        return res;
+    }
+};
