d41d37584996465ebe8c9d3f34fede24918a09dd
nextonesfaster <nextonesfaster@gmail.com>
2024-09-10 13:04:20 +0530

    add problems 64-66

---
+++ problems/problem_65.cpp
@@ -0,0 +1,28 @@
+// https://neetcode.io/problems/task-scheduling
+
+#include <algorithm>
+#include <vector>
+
+class Solution
+{
+public:
+    int leastInterval(std::vector<char> &tasks, int n)
+    {
+        int size = tasks.size();
+        int freq[26] = {0};
+        int max_freq = 0;
+        for (auto task : tasks)
+            max_freq = std::max(max_freq, ++freq[task - 'A']);
+
+        int max_n = std::count(freq, freq + 26, max_freq);
+
+        return std::max(size, (n + 1) * (max_freq - 1) + max_n);
+    }
+};
+
+int main()
+{
+    Solution sol;
+    std::vector<char> tasks = {'X', 'X', 'Y', 'Y'};
+    sol.leastInterval(tasks, 2);
+}
