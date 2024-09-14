08b6da3a926836af3e268f6cb823b1d1aa3c3518
nextonesfaster <nextonesfaster@gmail.com>
2024-09-15 01:52:25 +0530

    mark cell as visited before popping any

--- problems/problem_80.cpp
+++ problems/problem_80.cpp
@@ -24,18 +24,21 @@ public:
 
                 std::stack<std::pair<int, int>> stack;
                 stack.push({r, c});
+                grid[r][c] = '0';
 
                 while (!stack.empty())
                 {
                     auto [i, j] = stack.top();
-                    grid[i][j] = '0'; // mark as visited
                     stack.pop();
 
                     for (int k = 0; k < OFFSETS.size() - 1; k++)
                     {
                         int ni = i + OFFSETS[k], nj = j + OFFSETS[k + 1];
                         if (ni >= 0 && ni < M && nj >= 0 && nj < N && grid[ni][nj] == '1')
+                        {
                             stack.push({ni, nj});
+                            grid[ni][nj] = '0';
+                        }
                     }
                 }
 
