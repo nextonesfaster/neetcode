8a25786004c4d6eccb216d932aee1070ce374252
nextonesfaster <nextonesfaster@gmail.com>
2024-09-14 22:33:24 +0530

    add problems 76-78

---
+++ problems/problem_76.cpp
@@ -0,0 +1,61 @@
+// https://neetcode.io/problems/n-queens
+
+#include <vector>
+#include <string>
+#include <unordered_set>
+
+class Solution
+{
+    std::vector<std::vector<std::string>> res;
+    std::vector<int> constraints;
+    int n;
+
+    bool is_under_attack(int r, int c)
+    {
+        return constraints[c] || constraints[n + r + c] || constraints[4 * n - 2 + r - c];
+    }
+
+    void place_queen(int r, int c)
+    {
+        constraints[c] = constraints[n + r + c] = constraints[4 * n - 2 + r - c] = 1;
+    }
+
+    void remove_queen(int r, int c)
+    {
+        constraints[c] = constraints[n + r + c] = constraints[4 * n - 2 + r - c] = 0;
+    }
+
+    void backtrack(int r, std::vector<std::string> &board)
+    {
+        if (r == n)
+        {
+            res.push_back(board);
+            return;
+        }
+
+        for (int c = 0; c < n; c++)
+        {
+            if (is_under_attack(r, c))
+                continue;
+
+            place_queen(r, c);
+            board[r][c] = 'Q';
+
+            backtrack(r + 1, board);
+
+            remove_queen(r, c);
+            board[r][c] = '.';
+        }
+    }
+
+public:
+    std::vector<std::vector<std::string>> solveNQueens(int n)
+    {
+        this->n = n;
+        constraints = std::vector<int>(5 * n - 2, 0);
+        std::vector<std::string> board(n, std::string(n, '.'));
+
+        backtrack(0, board);
+        return res;
+    }
+};
---
+++ problems/problem_77.cpp
@@ -0,0 +1,71 @@
+// https://neetcode.io/problems/implement-prefix-tree
+
+#include <array>
+#include <string>
+
+class PrefixTree
+{
+    class TrieNode
+    {
+    public:
+        std::array<TrieNode *, 26> chars = {nullptr};
+        bool is_word = false;
+
+        TrieNode() {}
+        TrieNode(bool is_word) : is_word(is_word) {}
+    };
+
+    TrieNode *root;
+
+public:
+    PrefixTree()
+    {
+        root = new TrieNode();
+    }
+
+    void insert(std::string word)
+    {
+        TrieNode *curr = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (curr->chars[word[i] - 'a'])
+            {
+                curr = curr->chars[word[i] - 'a'];
+                if (i == word.length() - 1)
+                    curr->is_word = true;
+            }
+            else
+            {
+                TrieNode *node = new TrieNode(i == word.length() - 1);
+                curr->chars[word[i] - 'a'] = node;
+                curr = node;
+            }
+        }
+    }
+
+    bool search(std::string word)
+    {
+        TrieNode *curr = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (!curr->chars[word[i] - 'a'] || (i == word.length() - 1 && !curr->chars[word[i] - 'a']->is_word))
+                return false;
+            curr = curr->chars[word[i] - 'a'];
+        }
+
+        return true;
+    }
+
+    bool startsWith(std::string prefix)
+    {
+        TrieNode *curr = root;
+        for (int i = 0; i < prefix.length(); i++)
+        {
+            if (!curr->chars[prefix[i] - 'a'])
+                return false;
+            curr = curr->chars[prefix[i] - 'a'];
+        }
+
+        return true;
+    }
+};
