edd3780629ae3c9fb7a327d73337fa9567f7acf4
nextonesfaster <nextonesfaster@gmail.com>
2024-09-21 13:49:06 +0530

    add problems 89-90

---
+++ problems/problem_89.cpp
@@ -0,0 +1,53 @@
+// https://neetcode.io/problems/valid-tree
+
+#include <vector>
+#include <stack>
+
+class Solution
+{
+    bool has_cycle(std::vector<std::vector<int>> &adj_lst, std::vector<bool> &visited)
+    {
+        std::stack<std::pair<int, int>> stack;
+        stack.push({-1, 0});
+
+        while (!stack.empty())
+        {
+            auto [parent, node] = stack.top();
+            if (visited[node])
+                return true;
+
+            stack.pop();
+
+            visited[node] = true;
+            for (auto &child : adj_lst[node])
+            {
+                if (child != parent)
+                    stack.push({node, child});
+            }
+        }
+
+        return false;
+    }
+
+public:
+    bool validTree(int n, std::vector<std::vector<int>> &edges)
+    {
+        std::vector<std::vector<int>> adj_lst(n);
+
+        for (auto &pair : edges)
+        {
+            adj_lst[pair[0]].push_back(pair[1]);
+            adj_lst[pair[1]].push_back(pair[0]);
+        }
+
+        std::vector<bool> visited(n);
+        if (has_cycle(adj_lst, visited))
+            return false;
+
+        for (int i = 0; i < n; i++)
+            if (!visited[i])
+                return false;
+
+        return true;
+    }
+};
